5c612e1e66106d4b226ed9739a2f31a39a486d15
John Marshall <34883+swax@users.noreply.github.com>
2025-04-19 14:30:48 -0400

    Cached authentication record to disk

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import {
+  AuthenticationRecord,
   ClientSecretCredential,
   DeviceCodeCredential,
-  DeviceCodeInfo,
+  useIdentityPlugin,
 } from "@azure/identity";
+import { cachePersistencePlugin } from "@azure/identity-cache-persistence";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import fs from "fs";
 import { NotebookManagement } from "./functions/notebooks";
 import { PageManagement } from "./functions/pages";
 import { SectionManagement } from "./functions/sections";
@@ -21,15 +24,30 @@ export class OneNoteMCPServer extends McpServer {
       version: "1.0.0",
     });
 
-    const tenantId = process.env.AZURE_TENANT_ID;
     const clientId = process.env.AZURE_CLIENT_ID;
     const clientSecret = process.env.AZURE_CLIENT_SECRET;
 
-    if (!tenantId || !clientId || !clientSecret) {
+    if (!clientId || !clientSecret) {
       throw new Error(
         "Azure credentials must be provided via environment variables",
       );
     }
+
+    useIdentityPlugin(cachePersistencePlugin);
+
+    const authenticationRecordPath = ".cache/authentication-record.json";
+    let authenticationRecord: AuthenticationRecord | undefined = undefined;
+
+    // If we have an existing record, deserialize it.
+    if (fs.existsSync(authenticationRecordPath)) {
+      // authenticationRecord = AuthenticationRecord.deserialize(new FileInputStream(authenticationRecordPath));
+      authenticationRecord = JSON.parse(
+        fs.readFileSync(authenticationRecordPath, "utf-8"),
+      );
+      console.log("Loaded cached authentication record");
+    }
+
+    const tenantId = "consumers";
 
     this.credential = new ClientSecretCredential(
       tenantId,
@@ -38,16 +56,32 @@ export class OneNoteMCPServer extends McpServer {
     );
 
     this.deviceCodeCredential = new DeviceCodeCredential({
-      clientId: clientId,
-      tenantId: "consumers",
-      userPromptCallback: (info: DeviceCodeInfo) => {
+      clientId,
+      tenantId,
+      tokenCachePersistenceOptions: {
+        enabled: true,
+      },
+      authenticationRecord,
+      /*userPromptCallback: (info: DeviceCodeInfo) => {
         // Display the device code message to
         // the user. This tells them
         // where to go to sign in and provides the
         // code to use.
         console.log(info.message);
-      },
+      },*/
     });
+
+    const scope = "https://graph.microsoft.com/.default";
+
+    this.deviceCodeCredential.authenticate(scope).then((token) => {
+      // Save the authentication record to a file
+      console.log("Authenticated successfully");
+      fs.writeFileSync(authenticationRecordPath, JSON.stringify(token));
+    });
+
+    /*this.deviceCodeCredential
+      .getToken(scope)
+      .then((token) => console.log("GetToken: ", token));*/
 
     new NotebookManagement(this, this.deviceCodeCredential);
     new PageManagement(this, this.credential);
